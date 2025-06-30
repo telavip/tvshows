aba0a781c6dc9c284c65d5c3e04f4f900153ac27
telavip <megateca@outlook.com>
2025-06-30 19:09:46 -0300

    Add files via upload

---
+++ series7.js
@@ -0,0 +1,23 @@
+const series7 = [
+{
+  "id": 202555,
+  "slug": "demolidor-renascido",
+  "type": "serie",
+  "title": "Demolidor: Renascido",
+  "original_title": "Daredevil: Born Again",
+  "genre_ids": [
+    18,
+    80
+  ],
+  "first_air_date": "2025-03-04",
+  "popularity": 306.212,
+  "vote_average": 8.9,
+  "poster_path": "https://image.tmdb.org/t/p/w200/eODcpeaj6jFoVG1kBVpNFyFh45I.jpg",
+  "networks": [
+    2739
+  ],
+  "certification": "18",
+  "episode_count": 9
+},
+
+];
